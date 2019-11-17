2808ae60cd82bb02f9fda16b0e356b87469d7449
ricrdobah <rickribeiro99@gmail.com>
2019-11-17 14:16:41 +0000

    Chapter2

---
+++ Chapter2/Variables.py
@@ -0,0 +1,15 @@
+message = "riCardo ribeiro"
+print(message)
+print(message.title())
+print(message.upper())
+print(message.lower())
+first ="ricardo"
+second="ribeiro"
+print(first+" "+second)
+print((first+"\n\t"+second).title())
+print("ola ".rstrip()+"oi")  #remover espaço no final
+print(2**3)
+print(0.2+0.1)
+print(round(0.2+0.1,2))
+print(str(1))
+print( str(3/2) + " " + str(3.0/2))
