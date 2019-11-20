77fdd70e4bf520c14172d5ff30236157a7263eaf
ricrdobah <rickribeiro99@gmail.com>
2019-11-20 22:46:41 +0000

    finalizando listas

--- Chapter3/chapter4.py
+++ Chapter3/chapter4.py
@@ -1,3 +1,20 @@
 names = ['ricardo','arba','xhoerta']
 for name in names:
     print(name)
+    print("e")
+print("acabou a identação")
+
+numbers = list(range(1,5))
+for value in numbers:
+    print(value)
+square = list(range(1,7,2))
+print(square)
+numeros = [1,2,3,4,5,6,7,8,9]
+print(max(numeros))
+print(min(numeros))
+print(sum(numeros))
+print(numeros[0:3])
+print(numeros[:4])
+copy = numeros[:]#se nao botar p dar slice ele só passa a referencia
+print(copy)
+
