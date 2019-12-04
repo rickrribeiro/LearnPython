5eb26572adcb25986809f1f820f27634dc9bea0f
ricrdobah <rickribeiro99@gmail.com>
2019-12-04 22:10:08 +0000

    finishing classes module

--- classModule9/dog.py
+++ classModule9/dog.py
@@ -1,5 +1,25 @@
-class dog():
-
+class Dog():
     def __init__(self,name,age):
         self.name= name
         self.age=age
+
+    def info(self):
+        print(self.name+" "+str(self.age))
+    def latir(self):
+        print("au")
+#inheritance
+class security_dog(Dog):
+    def __init__(self,name,age):
+        super().__init__(name,age)
+    def latir(self): #override
+        print("au au")
+
+class canil():
+    def __init__(self,name):
+        self.name = name
+        self.dogs=[]
+    def addDog(self,dog):
+        self.dogs.append(dog)
+    def printDog(self):
+        for dog in self.dogs:
+            print(dog.name)
---
+++ classModule9/main.py
@@ -0,0 +1,18 @@
+import dog
+#from dog import Canil, dog
+#i can import a module inside another module
+my_dog = dog.Dog('Kiara',3)
+my_dog.info()
+print(my_dog.name)
+
+security = dog.security_dog('rotweiller',12)
+
+print(security.name)
+select=1
+canil = dog.canil("Canils")
+while select != 0:
+    select = int(input("1.Add\n2.List\n0.Exit\n"))
+    if select == 1:
+        canil.addDog(dog.Dog(input("Name: "),int(input("Age: "))))
+    elif select == 2:
+        canil.printDog()
