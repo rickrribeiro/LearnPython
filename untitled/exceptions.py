ccab26ee3619122bd6703959798c254d175420b7
ricrdobah <rickribeiro99@gmail.com>
2019-12-08 03:59:45 +0000

    try catch

---
+++ untitled/exceptions.py
@@ -0,0 +1,7 @@
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print("u cant divide by zero bro")
+else:
+    print("dividiu")
+
--- untitled/main.py
+++ untitled/main.py
@@ -1,16 +1,20 @@
 i=0
-with open('numbers') as file:
-    fil = file
-    content = fil.read()
-    lines = fil.readlines()
-    print(content.rstrip()) #rstrip is used to remove whitespaces from the right side of a string
-    if str(1) in content:
-        print("tem")
+try:
+    with open('numbers') as file:
+        fil = file
+        content = fil.read()
+        lines = fil.readlines()
+        print(content.rstrip()) #rstrip is used to remove whitespaces from the right side of a string
+        if str(1) in content:
+          print("tem")
 
-    print(lines)
-    for line in lines:
-        print(line)
-
+        print(lines)
+        for line in lines:
+           print(line)
+except FileNotFoundError:
+    print("arquivo n enontrado")
+except ZeroDivisionError:
+    pass
 #r - read, w - write, a - append (write without delete), r+ - read and write
 with open('numbers','a') as file:
     file.write('teste')
